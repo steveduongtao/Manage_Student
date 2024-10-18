098ca04a40021d0985974e9a6b0f3ae581b13482
Steve Duong <duongthetao1011995@gmail.com>
2024-10-18 17:09:30 +0700

    im back

--- src/components/Common/PrivateRoute.tsx
+++ src/components/Common/PrivateRoute.tsx
@@ -4,7 +4,13 @@ export function PrivateRoute() {
   // Check if user is logged in
   // If yes, show route
   // Otherwise, redirect to login page
-
+  const arrayA = [
+    { fielID: 'tt', headerName: 'thong tin' },
+    { fielID: 'tt1', headerName: 'thong tin1' },
+    { fielID: 'tt2', headerName: 'thong tin2' },
+  ];
+  const arrayB = arrayA.map((item) => item.fielID);
+  console.log('arrayB', arrayB);
   const isLoggedIn = Boolean(localStorage.getItem('access_token'));
   console.log('isLoginPrivteRoute', isLoggedIn);
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
